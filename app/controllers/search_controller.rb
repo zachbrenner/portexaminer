6a7f25008d56c13701ba2d61700ff6f11306569a
Zach <zachariahcagneybrenner@gmail.com>
2018-03-01 11:40:14 -0500

    process multiple pages and search from results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,24 +5,45 @@ class SearchController < ApplicationController
   end
   def search
   	@keywords = params[:keywords].split(",")
-  	@chart = {}
-  	@count = 1
-  	results = @keywords.each do |keyword|
-  		@count = 1
-		doc = Nokogiri::HTML(open("http://portexaminer.com/search.php?search-field-1=shipper&search-term-1=#{keyword}"))
+  	scrape_results(@keywords)
 
-		#puts doc.css("div[class=search-item]").css("div[class=blurb]")
-		doc.css("div[class=search-item]").each do |item|
-			blurb = item.css('div.blurb').children.text
-			next if blurb.include?("China") or blurb.include?("India")
-			title = item.css("div[class=title]").children.children.attribute('href').text[2..-1]
-			@chart[[@count,keyword,"http://" + title]] = blurb
-			#p title = item.css("div[class=title]")
-			#p title.attribute("div")
-			@count += 1
+  end	
+
+  def process_page(keyword,doc)
+
+	doc.css("div[class=search-item]").each do |item|
+		blurb = item.css('div.blurb').children.text
+		#next if blurb.include?("China") or blurb.include?("India")
+		title = item.css("div[class=title]").children.children.attribute('href').text[2..-1]
+		@chart[[@count,keyword,"http://" + title]] = blurb
+		#p title = item.css("div[class=title]")
+		#p title.attribute("div")
+		@count += 1
 		#puts item
 		end
-  	end
+  end
 
+
+  def scrape_results(keywords)
+  	@chart = {}
+  	@count = 1
+
+  	results = keywords.each do |keyword|
+  		@count = 1
+  		keyword.strip!
+		doc = Nokogiri::HTML(open("http://portexaminer.com/search.php?search-field-1=shipper&search-term-1=#{keyword}"))
+		puts "Processing #{keyword}"
+		#puts doc.css("div[class=search-item]").css("div[class=blurb]")
+		process_page(keyword, doc)
+		if doc.css("div.pager").xpath('p')[0] != nil
+			number_of_pages = doc.css("div.pager").xpath('p')[0].text.split(" ")[3].to_i
+			(2..number_of_pages).each do |page|
+				puts "Processing #{keyword} page #{page}"
+				page_doc = Nokogiri::HTML(open("http://portexaminer.com/search.php?search-field-1=shipper&search-term-1=#{keyword}&p=#{page}"))
+				process_page(keyword,page_doc)
+			end
+		end
+  	end
+  #	chart
   end
 end
